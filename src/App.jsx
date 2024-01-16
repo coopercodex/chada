82adc1331ecff9c78771d41f420363de520774a7
Derek Cooper <derekcooper20@gmail.com>
2024-01-15 18:32:35 -0700

    contact page and map

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom"
 import "./App.css"
-import { Contact } from "./components/Contact"
+import { Home } from "./components/Home"
 import { Footer } from "./components/Footer"
 import { ImageOne } from "./components/ImageOne"
 import { ImageThree } from "./components/ImageThree"
 import { ImageTwo } from "./components/ImageTwo"
 import { Navbar } from "./components/Navbar"
 import Menu from "./components/Menu"
+import { Contact } from "./components/Contact"
+// import { Home } from "@mui/icons-material"
 
 function App() {
   return (
@@ -18,7 +20,7 @@ function App() {
           element={
             <>
               <ImageOne />
-              <Contact />
+              <Home />
             </>
           }
         />
@@ -31,7 +33,15 @@ function App() {
             </>
           }
         />
-        {/* <ImageTwo /> */}
+        <Route
+          path="contact"
+          element={
+            <>
+              <ImageTwo />
+              <Contact />
+            </>
+          }
+        />
       </Routes>
       <Footer />
     </>
